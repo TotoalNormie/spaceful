cda1959846dcf4d0eaaa3952596788ca01838dd5
Vitaly Juno <vvjuhno@gmail.com>
2024-01-09 13:39:28 +0200

    assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom"
+import './style/main.css';
 import Test from "./components/Test"
+import Home from "./components/Home"
 
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route exact path='/' />
+      <Route exact path='/' element={<Home/>} />
       <Route exact path='/test' element={<Test/>} />
     </Routes>
     </BrowserRouter>
