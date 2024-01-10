b0c74234a86f1f2ce72022969989137ea3111226
Vitaly Juno <vvjuhno@gmail.com>
2024-01-10 10:36:55 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './style/main.css';
-<<<<<<< HEAD
 import Test from './components/Test';
 import Home from './components/Home';
 import Header from './components/Header';
 import Jabis from './components/Jabis';
+import Login from "./components/Login"
+import Register from "./components/Register"
 
 function App() {
 	return (
@@ -15,30 +16,13 @@ function App() {
           
 					<Route exact path='/' element={<Home />} />
 					<Route exact path='/test' element={<Test />} />
-					<Route exact path='/resns' element={<Jabis />} />
+					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/register' element={<Register />} />
+					<Route exact path='/resns' element={<Jabis />} /> {/* !!! OBLIGATI ATSTAT !!!*/}
 				</Routes>
 			</main>
 		</BrowserRouter>
 	);
-=======
-import Test from "./components/Test"
-import Home from "./components/Home"
-import Login from "./components/Login"
-import Register from "./components/Register"
-
-function App() {
-
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route exact path='/' element={<Home/>} />
-      <Route exact path='/test' element={<Test/>} />
-      <Route exact path='/login' element={<Login/>} />
-      <Route exact path='/register' element={<Register/>} />
-    </Routes>
-    </BrowserRouter>
-  )
->>>>>>> b73da54d555fa0508aa285cc6ae99bedbadab02a
 }
 
 export default App;
