f738c5b1df5cd6d8c4c544fc9af1eb4114697d7f
TTruble <blazingaustin@gmail.com>
2024-01-12 12:35:36 +0200

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,18 +3,12 @@ import css from '../style/Home.module.css';
 
 const Home = () => {
 	return (
-<<<<<<< HEAD
-		<section>
-			<p className={css.title}>spaceful</p>
-			<h1>the best solution for your warehouse management needs</h1>
-=======
 		<>
 		<section className={css.space}>
 			<div>
 				<p className={css.title}>spaceful</p>
-				<h1>the best solution for your warehouse manegment needs</h1>
+				<h1>the best solution for your warehouse management needs</h1>
 			</div>
->>>>>>> main
 		</section>
 		<section>
 
