ef0b6cc562e22ce5e27b645d5d292d5ff7bfe175
Vitaly Juno <vvjuhno@gmail.com>
2024-01-12 13:37:31 +0200

    landing page frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,10 @@ import Test from './components/Test';
 import Home from './components/Home';
 import Header from './components/Header';
 import Jabis from './components/Jabis';
-import Login from "./components/Login"
-import Register from "./components/Register"
-import AddNewProduct from "./components/AddNewProduct"
-import Logout from "./components/Logout"
-
+import Login from './components/Login';
+import Register from './components/Register';
+import AddNewProduct from './components/AddNewProduct';
+import Logout from './components/Logout';
 
 function App() {
 	return (
@@ -18,10 +17,10 @@ function App() {
 				<Routes>
 					<Route exact path='/' element={<Home />} />
 					<Route exact path='/test' element={<Test />} />
-					<Route exact path='/login' element={<Login/>} />
-          <Route exact path='/register' element={<Register/>} />
-          <Route exact path='/addnewproduct' element={<AddNewProduct/>} />
-          <Route exact path='/logout' element={<Logout/>} />
+					<Route exact path='/login' element={<Login />} />
+					<Route exact path='/register' element={<Register />} />
+					<Route exact path='/addnewproduct' element={<AddNewProduct />} />
+					<Route exact path='/logout' element={<Logout />} />
 					<Route exact path='/resns' element={<Jabis />} /> {/* !!! OBLIGATI ATSTAT !!!*/}
 				</Routes>
 			</main>
