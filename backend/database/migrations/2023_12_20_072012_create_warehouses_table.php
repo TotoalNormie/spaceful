152301c13dc5888aed3317e36c2a54aaf845079c
Vitaly Juno <vvjuhno@gmail.com>
2024-01-19 09:08:19 +0200

    database change

--- backend/database/migrations/2023_12_20_072012_create_warehouses_table.php
+++ backend/database/migrations/2023_12_20_072012_create_warehouses_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Products;
+use App\Models\warehouse_app;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration
         Schema::create('warehouses', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Products::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(warehouse_app::class)->constrained()->cascadeOnDelete();
             $table->string('shelfId', 25);
             $table->string('amount');
             $table->timestamps();
--- frontend/src/components/WarehouseApp.jsx
+++ frontend/src/components/WarehouseApp.jsx
@@ -4,7 +4,6 @@ import Cookies from 'js-cookie';
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-
 const WarehouseApp = () => {
 	const { warehouseId } = useParams();
 	const [name, setName] = useState('');
