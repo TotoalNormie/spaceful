557788e35c31a2643c682ded9c995bbbad3207c9
Vitaly Juno <vvjuhno@gmail.com>
2024-01-20 20:11:14 +0200

    working sidebar (really this time)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, NavLink, Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import './style/main.css';
 import Home from './components/Home';
 import Header from './components/Header';
@@ -12,40 +12,52 @@ import EditProfile from './components/EditProfile';
 import CreateWarehouse from './components/CreateWarehouse';
 import Error404 from './components/Error404';
 import Cookies from 'js-cookie';
-import ProductSearch from "./components/ProductSearch";
+import ProductSearch from './components/ProductSearch';
 import WarehouseApp from './components/WarehouseApp';
 import Sidebar from './components/Sidebar';
 import WarehouseWrapper from './components/WarehouseWrapper';
+import { useState } from 'react';
+
+const WarehouseCheck = ({ children }) => {
+	const location = useLocation();
+	const route = location.pathname.split('/')[1];
+	// console.log(location, route);
+	const isWarehouse = route === 'warehouse';
+	// console.log(isWarehouse);
+	return !isWarehouse && children;
+};
 
 function App() {
 	const isLoggedIn = Boolean(Cookies.get('token'));
-	const isWarehouse = () => {
-		const location = useLocation();
-		const route = location.pathname.split('/')[0];
-		console.log(route);
+	const [sidebarSeen, setSidebarSeen] = useState(false);
+
+	const toggleSidebar = () => {
+		setSidebarSeen(!sidebarSeen);
 	};
 	return (
 		<BrowserRouter>
-			<Header />
+			<Header hamburger={toggleSidebar} />
 			<main>
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Register />} />
-					<Route path='/addnewproduct' element={<AddNewProduct />} />
+					{/* <Route path='/addnewproduct' element={<AddNewProduct />} /> */}
 					<Route path='/resns' element={<Jabis />} />
-					<Route path='/addtowarehouse' element={<AddToWarehouse />} />
+					{/* <Route path='/addtowarehouse' element={<AddToWarehouse />} />
 					<Route path='/reports' element={<Reports />} />
+					<Route exact path='/ProductSearch' element={<ProductSearch />} /> */}
 					{isLoggedIn
 						? [
 								<Route
 									path='/warehouse/:warehouseId/*'
-									element={<WarehouseWrapper />}>
+									element={<WarehouseWrapper seen={sidebarSeen} />}>
 									<Route index element={<WarehouseApp />} />
+									<Route path='main' element={<WarehouseApp />} />
 									<Route path='addnewproduct' element={<AddNewProduct />} />
 									<Route path='addtowarehouse' element={<AddToWarehouse />} />
 									<Route path='reports' element={<Reports />} />
-                  <Route path='productsearch' element={<ProductSearch/>} />
+									<Route path='productsearch' element={<ProductSearch />} />
 								</Route>,
 								<Route path='/createwarehouse' element={<CreateWarehouse />} />,
 								<Route exact path='/editprofile' element={<EditProfile />} />,
@@ -65,11 +77,11 @@ function App() {
 								/>,
 						  ]}
 					<Route path='*' element={<Error404 />} />
-		  <Route exact path='/ProductSearch' element={<ProductSearch/>} />
 				</Routes>
+				<WarehouseCheck>
+					<Sidebar seen={sidebarSeen}></Sidebar>
+				</WarehouseCheck>
 			</main>
-			<Sidebar />
-			{/* {isLoggedIn ? <Sidebar /> : null} */}
 		</BrowserRouter>
 	);
 }
